2530a4674d34d036619b14fd95517deae704dab4
Max Levitskiy <max.dstu@gmail.com>
2024-02-18 08:17:31 +0100

    s3

--- indexer/index.js
+++ indexer/index.js
@@ -1,8 +1,24 @@
 // serve http requests
 import express from 'express';
 import weaviate from 'weaviate-ts-client';
+import fs from 'fs';
+import {GetObjectCommand, S3Client} from "@aws-sdk/client-s3";
+
+import dotenv from 'dotenv';
+dotenv.config();
+
 const app = express();
 const port = 4000;
+
+const client = weaviate.client({
+    scheme: 'http',
+    host: '137.184.41.20:8080',
+});
+const s3client = new S3Client({
+    region: process.env.AWS_DEFAULT_REGION || "eu-central-1",
+});
+const s3Bucket = "uniqverse";
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
@@ -18,9 +34,8 @@ app.get('/', (req, res) => {
 //     // .withLimit(1)
 //     .do();
 
-// get the file, take sha256 hash and save it to the filesystem
 app.post('/upload', (req, res) => {
-    const fs = require('fs');
+    console.log(req.files.image)
     const { image } = req.body;
     const buffer = Buffer.from(image, 'base64');
     fs.writeFileSync('nfts/image.jpg', buffer);
@@ -29,16 +44,22 @@ app.post('/upload', (req, res) => {
 
 
 app.post('/similar', async (req, res) => {
-    const client = weaviate.client({
-        scheme: 'http',
-        host: '137.184.41.20:8080',
-    });
+    console.log(req.body)
     const { image, distance } = req.body;
-    const test = Buffer.from(image, 'base64').toString('base64');
+    const cmd = new GetObjectCommand({
+        Bucket: s3Bucket,
+        Key: `nfts/${image}`
+    })
+    const nftResp = await s3client.send(cmd);
+    console.log(nftResp);
+    // read nftResp.Body stream as base64
+
+    let str = await nftResp.Body.transformToByteArray();
+    const b64 = Buffer.from(str).toString('base64');
     const resImage = await client.graphql.get()
         .withClassName('NFTs')
         .withFields('name, _additional { id distance }')
-        .withNearImage({ image: test, distance: distance })
+        .withNearImage({ image: b64, distance: distance })
         .do();
     res.send(resImage.data.Get.NFTs);
 });
@@ -54,10 +75,25 @@ app.post('/similar', async (req, res) => {
 //     }),
 // });
 
+// get image url, download image and index it in the weaviate
+app.post('/index', async (req, res) => {
+    const { imageUrl, name } = req.body;
+    const buffer = await (await fetch(imageUrl)).buffer();
+    const b64 = Buffer.from(buffer).toString('base64');
+    await client.data.creator()
+        .withClassName('NFTs')
+        .withProperties({
+            image: b64,
+            name: name,
+        })
+        .do();
+    res.send('ok');
+});
+
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
-
 // error handling
 app.use((err, req, res, next) => {
     console.error(err.stack);
